b6cdcef97c322b7f562f75a05e316e9c5d8f7ea5
Mugil99 <mugilya99@gmail.com>
2023-06-04 14:28:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes,Route } from 'react-router-dom';
+import HomePage from './Components/HomePage/HomePage';
+import DetailPage from './Components/DetailsPage/DetailPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+  return(
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/item/:id" element={<DetailPage />} />
+    </Routes>
+  )
 }
-
 export default App;
